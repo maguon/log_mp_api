a183ae5e5f55ecccc8467f62cc94d0ed35d133c3
maguon <info@myxxjs.com>
2018-08-23 14:31:23 +0800

    reset schema user privileges

--- db/sql/create_schema.sql
+++ db/sql/create_schema.sql
@@ -1,4 +1,4 @@
 CREATE SCHEMA `log_mp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'log'@'%' IDENTIFIED BY 'log_mp';
+CREATE USER 'mp_db_user'@'%' IDENTIFIED BY 'log_mp_2018';
 
-GRANT ALL privileges ON log_base.* TO 'log'@'%'IDENTIFIED BY 'log_mp';
+GRANT ALL privileges ON log_mp.* TO 'mp_db_user'@'%'IDENTIFIED BY 'log_mp_2018';
