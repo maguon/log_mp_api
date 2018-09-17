e91bb1084385746a15f8f1d32d7ec76b88bfe3fa
yangguoquan511 <guoquan.yang@myxxjs.com>
2018-09-17 15:49:26 +0800

    改代码

--- bl/User.js
+++ bl/User.js
@@ -20,43 +20,40 @@ const updateUser = (req,res,next)=>{
              return next();
          }
      });
-
 };
 const updatePassword=(req,res,next)=>{
     let params = req.params;
-    userDao.queryUser(params,(error,rows)=>{
-        if(error){
-            logger.error('updatePassword' + error.message);
-            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
-        }else{
-            if(rows && rows.length < 1){
+    new Promise((resolve) => {
+        userDao.queryUser(params,(error,rows)=>{
+            if(error){
+                logger.error('updatePassword' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            }else if(rows && rows.length < 1){
                 logger.warn('updatePassword' + "尚未注册");
                 resUtil.resetFailedRes(res,"尚未注册");
                 return next();
-            }else
-                var md5Password = encrypt.encryptByMd5(params.oldPassword);
-                //console.log(md5Password);
-                if(md5Password != rows[0].password){
-                    //console.log(rows[0].password);
+            }else if(encrypt.encryptByMd5(params.oldPassword) != rows[0].password){
                 logger.warn('updatePassword' + "原密码错");
                 resUtil.resetFailedRes(res,"原密码错误");
                 return next();
             }else{
-                params.newPassword = encrypt.encryptByMd5(params.newPassword);
-                userDao.updatePassword(params,(error,result)=>{
-                    if(error){
-                        logger.error('updatePassword' + error.message);
-                        throw sysError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
-                    }else{
-                        logger.info('updatePassword' + 'success');
-                        resUtil.resetUpdateRes(res,result,null);
-                        return next();
-                    }
-                })
+                resolve(encrypt.encryptByMd5(params.newPassword));
             }
-        }
-    });
-};
+        })
+    }).then(() => {
+        params.newPassword = encrypt.encryptByMd5(params.newPassword);
+        userDao.updatePassword(params,(error,result)=>{
+            if(error){
+                logger.error('updatePassword' + error.message);
+                throw sysError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            }else{
+                logger.info('updatePassword' + 'success');
+                resUtil.resetUpdateRes(res,result,null);
+                return next();
+            }
+        })
+    })
+}
 const updateStatus=(req,res,next)=>{
     var params = req.params;
     userDao.updateStatus(params,(error,result)=>{
@@ -98,41 +95,43 @@ const queryUser = (req,res,next)=>{
 };
 const userLogin = (req,res,next)=>{
     let params = req.params;
-     userDao.getUser({wechatId:params.wechatId},(error,rows)=>{
-        if(error){
-            logger.error('userLogin'+error.message);
-            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
-        }else{
-            if(rows && rows.length < 1){
-                params.password = encrypt.encryptByMd5(params.password)
-                userDao.createUser(params,(error,result)=>{
-                    if(error) {
-                        logger.error('createUser' + error.message);
-                        throw sysError.InternalError(error.message, sysMsg.SYS_INTERNAL_ERROR_MSG);
-                    }
-                    else
-                        if (result && result != null) {
-                            logger.info('create' + 'success');
-                            resUtil.resetCreateRes(res,result,null);
-                            return next();
-                            }
-                });
-            } else{
-                const resObj ={
-                    userId: rows[0].id,
-                    wechatAccount : rows[0].wechat_account,
-                    phone : rows[0].phone
+    new Promise((resolve)=>{
+        userDao.getUser({wechatId:params.wechatId},(error,rows)=>{
+            if(error){
+                logger.error('userLogin'+error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            }else {
+                if(rows && rows.length < 1){
+                    params.password = encrypt.encryptByMd5(params.password);
+                    resolve(params.password);
+                }else{
+                    let resObj ={
+                        userId: rows[0].id,
+                        wechatAccount : rows[0].wechat_account,
+                        phone : rows[0].phone
+                    };
+                    resUtil.resetQueryRes(res,resObj,null);
+                    var myDate = new Date();
+                    params.lastLoginOn = myDate;
+                    userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
+                    return next();
                 }
-                resUtil.resetQueryRes(res,resObj,null);
-                var myDate = new Date();
-                params.lastLoginOn = myDate;
-                userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
+            }
+        })
+    }).then(()=>{
+        userDao.createUser(params,(error,result)=>{
+            if(error) {
+                logger.error('createUser' + error.message);
+                throw sysError.InternalError(error.message, sysMsg.SYS_INTERNAL_ERROR_MSG);
+            }
+            else{
+                logger.info('create' + 'success');
+                resUtil.resetCreateRes(res,result,null);
                 return next();
             }
-        }
-    });
+        });
+    })
 };
-
 module.exports ={
     queryUser,
     userLogin,
