4beacba7c7caa30c1d6d85296749df628bfa799f
yangguoquan511 <guoquan.yang@myxxjs.com>
2018-09-30 15:11:37 +0800

    adminUser

---
+++ bl/AdminUser.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const sysMsg = require('../util/SystemMsg.js');
+const sysError = require('../util/SystemError.js');
+const encrypt = require('../util/Encrypt.js');
+const resUtil = require('../util/ResponseUtil.js');
+const listOfValue = require('../util/ListOfValue.js');
+const oAuthUtil = require('../util/OAuthUtil.js');
+const adminUserDao = require('../dao/AdminUserDAO.js');
+const serverLogger = require('../util/ServerLogger.js');
+const logger = serverLogger.createLogger('Admin.js');
+
+const createAdminUser = (req,res,next) => {
+    let params = req.params;
+    new Promise((resolve,reject)=>{
+        adminUserDao.queryAdminUser({phone:params.phone},(error,rows)=>{
+            if (error) {
+                logger.error(' queryAdminUser ' + error.message);
+                resUtil.resetFailedRes(res,sysMsg.SYS_INTERNAL_ERROR_MSG) ;
+                return next();
+            } else {
+                if(rows && rows.length>0){
+                    logger.warn(' queryAdminUser ' +params.phone+ sysMsg.CUST_SIGNUP_REGISTERED);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_SIGNUP_REGISTERED) ;
+                    return next();
+                }else{
+                    resolve();
+                }
+            }
+        })
+    }).then(()=>{
+        params.password = encrypt.encryptByMd5(params.password);
+        adminUserDao.createAdminUser(params,(error,result)=>{
+            if (error) {
+                logger.error(' createAdminUser ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(result && result.insertId>0){
+                    logger.info(' createAdminUser ' + 'success');
+                    let user = {
+                        userId : result.insertId,
+                        userStatus : listOfValue.USER_STATUS_ACTIVE
+                    }
+                    user.accessToken = oAuthUtil.createAccessToken(oAuthUtil.clientType.user,user.userId,user.userStatus);
+                    resUtil.resetQueryRes(res,user,null);
+                }else{
+                    logger.warn(' createAdminUser ' + 'false');
+                    resUtil.resetFailedRes(res,sysMsg.SYS_INTERNAL_ERROR_MSG);
+                }
+                return next();
+            }
+        })
+    })
+}
+const adminUserLogin = (req,res,next) =>{
+    let params = req.params;
+    adminUserDao.queryAdminUser(params,(error,rows)=>{
+        if (error) {
+            logger.error(' adminUserLogin ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            if(rows && rows.length<1){
+                logger.warn(' adminUserLogin ' +params.userName+ sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                resUtil.resetFailedRes(res,sysMsg.ADMIN_LOGIN_USER_UNREGISTERED) ;
+                return next();
+            }else{
+                let passwordMd5 = encrypt.encryptByMd5(params.password);
+                if(passwordMd5 != rows[0].password){
+                    logger.warn(' adminUserLogin ' +params.phone+ sysMsg.CUST_LOGIN_PSWD_ERROR);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_LOGIN_PSWD_ERROR) ;
+                    return next();
+                }else{
+                    if(rows[0].status == listOfValue.ADMIN_USER_STATUS_NOT_ACTIVE){
+                        let user = {
+                            userId : rows[0].id,
+                            userStatus : rows[0].status
+                        }
+                        logger.info('adminUserLogin' +params.userName+ " not verified");
+                        resUtil.resetQueryRes(res,user,null);
+                        return next();
+                    }else{
+                        let user = {
+                            userId : rows[0].id,
+                            userStatus : rows[0].status
+                        }
+                        user.accessToken = oAuthUtil.createAccessToken(oAuthUtil.clientType.admin,user.userId,user.userStatus);
+                        logger.info('adminUserLogin' +params.userName+ " success");
+                        resUtil.resetQueryRes(res,user,null);
+                        return next();
+                    }
+                }
+            }
+        }
+    })
+}
+const getAdminUserInfo = (req,res,next) => {
+    let params = req.params;
+    adminUserDao.queryAdminInfo(params,(error,rows)=>{
+        if (error) {
+            logger.error(' getAdminUserInfo ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            logger.info(' getAdminUserInfo ' + 'success');
+            resUtil.resetQueryRes(res,rows,null);
+            return next();
+        }
+    })
+}
+const updateAdminInfo = (req,res,next) => {
+    let params = req.params;
+    adminUserDao.updateInfo(params,(error,result)=>{
+        if (error) {
+            logger.error(' updateAdminInfo ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            logger.info(' updateAdminInfo ' + 'success');
+            resUtil.resetUpdateRes(res,result,null);
+            return next();
+        }
+    })
+}
+const changeAdminPassword = (req,res,next) => {
+    let params = req.params;
+    new Promise((resolve,reject) => {
+        adminUserDao.queryAdminUser(params,(error,rows)=>{
+            if (error) {
+                logger.error(' changeAdminPassword ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(rows && rows.length<1){
+                    logger.warn(' changeAdminPassword ' + sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                    resUtil.resetFailedRes(res,sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                    return next();
+                }else if(encrypt.encryptByMd5(params.originPassword) != rows[0].password){
+                    logger.warn(' changeAdminPassword ' + sysMsg.CUST_ORIGIN_PSWD_ERROR);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_ORIGIN_PSWD_ERROR);
+                    return next();
+                }else{
+                    resolve();
+                }
+            }
+        })
+    }).then(() => {
+        params.password = encrypt.encryptByMd5(params.newPassword);
+        adminUserDao.updatePassword(params,(error,result)=>{
+            if (error) {
+                logger.error(' changeAdminPassword ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                logger.info(' changeAdminPassword ' + 'success');
+                resUtil.resetUpdateRes(res,result,null);
+                return next();
+            }
+        })
+    })
+}
+module.exports = {
+    createAdminUser,
+    adminUserLogin,
+    getAdminUserInfo,
+    updateAdminInfo,
+    changeAdminPassword
+}
---
+++ dao/AdminUserDAO.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const db=require('../db/connection/MysqlDb.js');
+const serverLogger = require('../util/ServerLogger.js');
+const logger = serverLogger.createLogger('AdminUserDAO.js');
+
+const createAdminUser = (params,callback) => {
+    let query = " insert into admin_user (user_name,real_name,password,phone,status) values ( ? , ? , ? , ? , ? )";
+    let paramsArray=[],i=0;
+    paramsArray[i++]=params.userName;
+    paramsArray[i++]=params.realName;
+    paramsArray[i++]=params.password;
+    paramsArray[i++]=params.phone;
+    paramsArray[i]=params.status;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' createAdminUser ');
+        return callback(error,rows);
+    });
+}
+const queryAdminUser = (params,callback) => {
+    let query = " select * from admin_user where id is not null ";
+    let paramsArray=[],i=0;
+    if(params.adminId){
+        paramsArray[i++] = params.adminId;
+        query = query + " and id = ? ";
+    }
+    if(params.userName){
+        paramsArray[i++] = params.userName;
+        query = query + " and user_name = ? ";
+    }
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' queryAdminUser ');
+        return callback(error,rows);
+    });
+}
+const queryAdminInfo = (params,callback) => {
+    let query = " select * from admin_user where id is not null";
+    let paramsArray=[],i=0;
+    if(params.adminId){
+        query = query + " and id = ? ";
+        paramsArray[i++]=params.adminId;
+    }
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' queryUser ');
+        return callback(error,rows);
+    });
+}
+const updateInfo = (params,callback) => {
+    let query = " update admin_user set real_name = ? ,phone = ? where id = ?";
+    let paramsArray=[],i=0;
+    paramsArray[i++] = params.realName;
+    paramsArray[i++] = params.phone;
+    paramsArray[i] = params.adminId;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' updateInfo ');
+        return callback(error,rows);
+    });
+}
+const updatePassword = (params,callback) => {
+    let query = " update admin_user set password = ? where id = ?";
+    let paramsArray=[],i=0;
+    paramsArray[i++] = params.password;
+    paramsArray[i] = params.adminId;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' updatePassword ');
+        return callback(error,rows);
+    });
+
+}
+
+module.exports = {
+    createAdminUser,
+    queryAdminUser,
+    queryAdminInfo,
+    updateInfo,
+    updatePassword
+}
