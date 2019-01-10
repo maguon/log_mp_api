9e3b9a617365d16d8d04f27190f3360b4c395334
admin <paiyi.zhang@myxxjs.com>
2019-01-10 16:22:47 +0800

    修改删除询价车辆接口

--- bl/InquiryCar.js
+++ bl/InquiryCar.js
@@ -61,7 +61,7 @@ const addCar = (req,res,next) => {
             })
         }).then(()=>{
             new Promise((resolve,reject)=>{
-                inquiryCarDAO.getSumPrice({inquiryId:params.inquiryId},(error,rows)=>{
+                inquiryCarDAO.getSumPrice({inquiryId:params.inquiryId,status:sysConsts.CAR.inquiryStatus.showInUser},(error,rows)=>{
                     if(error){
                         logger.error('getSumPrice' + error.message);
                         reject(error);
