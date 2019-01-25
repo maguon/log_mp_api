ccd9201e291258c34a67d6c8ba34dfeab29fa3bb
admin <paiyi.zhang@myxxjs.com>
2019-01-25 16:38:25 +0800

    算单价

--- bl/TransAndInsurePrice.js
+++ bl/TransAndInsurePrice.js
@@ -13,8 +13,8 @@ const transAndInsurePrice = (req,res,next) => {
         resUtil.resetFailedRes(res,sysMsg.GET_TRANS_AND_INSURE_PRICE);
     } else {
         logger.info('transAndInsurePrice' + 'success');
-        price.insure = price.insure.toFixed(2);
-        price.trans = price.trans.toFixed(2);
+        price.insure = parseFloat(price.insure.toFixed(2));
+        price.trans = parseFloat(price.trans.toFixed(2));
         resUtil.resetQueryRes(res,price,null);
         return next();
     }
