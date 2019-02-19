b08684fa9b7b32b581c7b8ce836c63af63a484e5
admin <paiyi.zhang@myxxjs.com>
2019-02-19 13:20:39 +0800

    修改路径

--- server.js
+++ server.js
@@ -175,8 +175,8 @@ const createServer=()=>{
     server.put({path:'/api/user/:userId/order/:orderId/sendMsg',contentType: 'application/json'},inquiryOrder.updateById);
     server.put({path:'/api/user/:userId/order/:orderId/recvMsg',contentType: 'application/json'},inquiryOrder.updateById);
     server.put({path:'/api/admin/:adminId/order/:orderId/selfMentionAddress',contentType: 'application/json'},inquiryOrder.selfMentionAddress);
-    server.get('/api/admin/:adminId/getOrderProfit',inquiryOrder.getOrderProfit);
-    server.get('/api/admin/:adminId/order/:orderId/getOrderCostOfCar',inquiryOrder.getOrderCostOfCar);
+    server.get('/api/admin/:adminId/orderProfit',inquiryOrder.getOrderProfit);
+    server.get('/api/admin/:adminId/order/:orderId/orderCostOfCar',inquiryOrder.getOrderCostOfCar);
     /**
      order_item
      */
