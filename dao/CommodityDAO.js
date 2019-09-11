43f7220c3a8e8e55d7c671886f1c7095e7e08ef4
yingmei567 <yingmei.yan@myxxjs.com>
2019-09-11 10:30:17 +0800

    Add status asc

--- dao/CommodityDAO.js
+++ dao/CommodityDAO.js
@@ -31,7 +31,7 @@ const getCommodity = (params,callback) => {
         paramsArray[i++] = params.showStatus;
         query = query + " and show_status = ? "
     }
-    query = query + " order by id desc";
+    query = query + " order by id desc, status asc";
     if(params.start && params.size){
         paramsArray[i++] = parseInt(params.start);
         paramsArray[i] = parseInt(params.size);
