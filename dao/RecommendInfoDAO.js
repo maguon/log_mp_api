0c20ae70121efc1a77fb28f376f1a17516402d86
yingmei567 <yingmei.yan@myxxjs.com>
2019-09-17 16:45:37 +0800

    Add pageUrl.

--- dao/RecommendInfoDAO.js
+++ dao/RecommendInfoDAO.js
@@ -4,24 +4,21 @@ const logger = serverLogger.createLogger('RecommendInfoDAO.js');
 const db = require('../db/connection/MysqlDb.js');
 
 const add = (params,callback) => {
-    let query = " insert into recommend_info (admin_id,name,introduction) values (?,?,?)";
+    let query = " insert into recommend_info (admin_id,name,introduction,page_url) values (?,?,?,?)";
     let paramsArray = [],i=0;
     paramsArray[i++] = params.adminId;
     paramsArray[i++] = params.name;
-    paramsArray[i] = params.introduction;
+    paramsArray[i++] = params.introduction;
+    paramsArray[i] = params.pageUrl;
     db.dbQuery(query,paramsArray,(error,rows)=>{
         logger.debug('add');
         callback(error,rows);
     })
 }
 const select = (params,callback) => {
-    let query = " select id,admin_id,name,introduction,content,status,mp_url,created_on,updated_on from recommend_info";
+    let query = " select * from recommend_info";
     query += " where 1=1";
     let paramsArray = [],i=0;
-    if (params.adminId){
-        paramsArray[i++] = params.adminId;
-        query += " and admin_id = ?";
-    }
     if (params.recommendId){
         paramsArray[i++] = params.recommendId;
         query += " and id = ?";
