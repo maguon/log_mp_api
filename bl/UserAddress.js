2d78a75ff7dcd98054d7c228f7197356b7f596b6
yingmei567 <yingmei.yan@myxxjs.com>
2019-09-29 10:39:59 +0800

    Update require.

--- bl/UserAddress.js
+++ bl/UserAddress.js
@@ -2,8 +2,6 @@
 
 const serverLogger = require('../util/ServerLogger.js');
 const resUtil = require('../util/ResponseUtil.js');
-const sysMsg = require('../util/SystemMsg.js');
-const sysError = require('../util/SystemError.js');
 const logger = serverLogger.createLogger('UserAddress.js');
 const userAddressDAO = require('../dao/UserAddressDAO.js');
 const sysConsts = require("../util/SystemConst");
